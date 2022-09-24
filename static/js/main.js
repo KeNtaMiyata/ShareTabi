39adf25207710d8b78897f01046dc5c8d123d269
KeM <kenta0802human@outlook.jp>
2022-09-25 05:19:15 +0900

    [add] new.html

--- static/js/main.js
+++ static/js/main.js
@@ -14,3 +14,40 @@ window.twttr = (function(d, s, id) {
   
     return t;
   }(document, "script", "twitter-wjs"));
+
+
+// for form js
+// https://stackoverflow.com/questions/31224651/show-hide-password-onclick-of-button-using-javascript-only
+
+function show() {
+  var p = document.getElementById('pwd');
+  p.setAttribute('type', 'text');
+}
+
+function hide() {
+  var p = document.getElementById('pwd');
+  p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+  if (pwShown == 0) {
+      pwShown = 1;
+      show();
+  } else {
+      pwShown = 0;
+      hide();
+  }
+}, false);
+
+
+// 添付した画像を表示
+function previewImage(obj)
+{
+	var fileReader = new FileReader();
+	fileReader.onload = (function() {
+		document.getElementById('preview').src = fileReader.result;
+	});
+	fileReader.readAsDataURL(obj.files[0]);
+}
